25061e6ac3da8de7f3d0c23a9bc5fd1df655272d
alchemist85k <alchemist85k@gmail.com>
2014-09-01 12:21:08 +0900

    make index view. only print

--- src/studentAdmin/CommonFunc.java
+++ src/studentAdmin/CommonFunc.java
@@ -1,12 +1,25 @@
 package studentAdmin;
 
 public class CommonFunc {
+	
+	public CommonFunc() {
+		mainView();
+	}
+	
 	/*
 	 * common function 
 	 */
 	
 	public void mainView() {
-		
+		System.out.println();
+		System.out.println("===== Student Administrator =====");
+		System.out.println("\t 1. create");
+		System.out.println("\t 2. update");
+		System.out.println("\t 3. delete");
+		System.out.println("\t 4. search");
+		System.out.println("\t 5. all print");
+		System.out.println("\t 0. exit");
+		System.out.print(" please enter number ::: ");
 	}
 	
 	public void addStudent() {
--- src/studentAdmin/Main.java
+++ src/studentAdmin/Main.java
@@ -3,8 +3,7 @@ package studentAdmin;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO do main
-
+		CommonFunc cf = new CommonFunc();
 	}
 
 }
