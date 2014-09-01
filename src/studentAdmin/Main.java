5b8075cd28d41065988b2e28f1549626b4718d44
alchemist85k <alchemist85k@gmail.com>
2014-09-01 12:11:38 +0900

    create Student class - values, getter and setter

--- src/studentAdmin/Main.java
+++ src/studentAdmin/Main.java
@@ -3,7 +3,7 @@ package studentAdmin;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO do main
 
 	}
 
---
+++ src/studentAdmin/Student.java
@@ -0,0 +1,45 @@
+package studentAdmin;
+
+public class Student {
+	
+	/*
+	 * administrator values 
+	 */
+	private int 	number;
+	private String 	name;
+	private String 	phone;
+	private String 	grade;
+	
+	/*
+	 * getter / setter 
+	 */
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+}
