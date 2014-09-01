9d1edf110dcb8558b20149dd31f85f788f76f13c
alchemist85k <alchemist85k@gmail.com>
2014-09-01 12:15:22 +0900

    make function skin

---
+++ src/studentAdmin/CommonFunc.java
@@ -0,0 +1,32 @@
+package studentAdmin;
+
+public class CommonFunc {
+	/*
+	 * common function 
+	 */
+	
+	public void mainView() {
+		
+	}
+	
+	public void addStudent() {
+		
+	}
+	
+	public void deleteStudent() {
+		
+	}
+	
+	public void updateStudent() {
+		
+	}
+	
+	public void searchStudent() {
+		
+	}
+	
+	public void allSearch() {
+		
+	}
+	
+}
